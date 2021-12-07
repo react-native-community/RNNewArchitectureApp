c89337080a47773afc7e856334e21dd6b7d6567a
Nicola Corti <corti.nico@gmail.com>
2021-12-07 16:10:31 +0000

    [Fabric] Create the corresponding ViewManager

---
+++ android/app/src/main/java/com/rnnewarchitectureapp/components/AnswerViewer.java
@@ -0,0 +1,22 @@
+package com.rnnewarchitectureapp.components;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+
+public class AnswerViewer extends androidx.appcompat.widget.AppCompatTextView {
+
+  public AnswerViewer(Context context) {
+    super(context);
+  }
+
+  public AnswerViewer(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public AnswerViewer(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+}
