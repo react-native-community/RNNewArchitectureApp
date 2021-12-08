12c239a6a54ebbfe87d14ed8a63864fbdfab57d7
Nicola Corti <corti.nico@gmail.com>
2021-12-08 16:58:16 +0000

    [Fabric] Use the new Fabric custom component

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@
  * @format
  */
 
-import React from 'react';
+import * as React from 'react';
+import {useRef, useState} from 'react';
 import type {Node} from 'react';
 import NativeAnswerSolver from './js/NativeAnswerSolver';
 import {
@@ -29,6 +30,8 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import AnswerViewer, {Commands as AnswerViewerCommands} from './js/NativeAnswerViewer';
+import type {AnswerViewerViewType} from './js/NativeAnswerViewer';
 
 const Section: React.FC<{
   title: string;
@@ -65,15 +68,21 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const ref = useRef<React.ElementRef<AnswerViewerViewType> | null>(null);
+  const [step, setStep] = useState(1);
+
   const onPress = () => {
     const theAnswer = NativeAnswerSolver?.answerTheUltimateQuestion("What's the Answer to the Ultimate Question of Life, the Universe, and Everything") || ""
     console.log('The answer is: ' + theAnswer);
+    setStep(step+1);
+    AnswerViewerCommands.changeBackgroundColor(ref.current, "red");
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Button title="Click to invoke your Turbo Module!" onPress={onPress} />
+      <AnswerViewer ref={ref} style={{width: "100%", height: 50}} color="yellow" step={step} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
