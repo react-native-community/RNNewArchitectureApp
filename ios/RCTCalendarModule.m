733ac336d8862c80c080ad8f88bc072364f41f25
Dmitry Rykun <dmitryrykun@fb.com>
2022-02-16 12:16:28 +0000

    [Native Modules] Export a Native Method to JavaScript

--- ios/RCTCalendarModule.m
+++ ios/RCTCalendarModule.m
@@ -6,10 +6,16 @@
 //
 
 #import "RCTCalendarModule.h"
+#import <React/RCTLog.h>
 
 @implementation RCTCalendarModule
 
 // To export a module named RCTCalendarModule
 RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
 
 @end
