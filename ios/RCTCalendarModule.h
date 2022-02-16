a9ea7d25244a8bb0448ea6b262be1d3451f3861c
Dmitry Rykun <dmitryrykun@fb.com>
2022-02-16 12:12:04 +0000

    [Native Modules] Create Custom Native Module Files

---
+++ ios/RCTCalendarModule.h
@@ -0,0 +1,17 @@
+//
+//  RCTCalendarModule.h
+//  
+//
+//  Created by Dmitry Rykun on 16/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTCalendarModule : NSObject <RCTBridgeModule>
+@end
+
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,15 @@
+//
+//  RCTCalendarModule.m
+//  
+//
+//  Created by Dmitry Rykun on 16/02/2022.
+//
+
+#import "RCTCalendarModule.h"
+
+@implementation RCTCalendarModule
+
+// To export a module named RCTCalendarModule
+RCT_EXPORT_MODULE();
+
+@end
