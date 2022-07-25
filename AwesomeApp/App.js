8e4ad5fcf50b66ed35849be2a331fdf5fc34a242
Riccardo Cipolleschi <cipolleschi@fb.com>
2022-07-25 16:32:21 +0100

    [TurboModule - Shared] Test the TurboModule

--- AwesomeApp/App.js
+++ AwesomeApp/App.js
@@ -1,112 +1,38 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+  * Sample React Native App
+  * https://github.com/facebook/react-native
+  *
+  * @format
+  * @flow strict-local
+  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
+  import React from 'react';
+  import {useState} from "react";
+  import type {Node} from 'react';
+  import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
+  Button,
   View,
-} from 'react-native';
+  } from 'react-native';
+  import Calculator from 'calculator/src/NativeCalculator';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const App: () => Node = () => {
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [result, setResult] = useState<number | null>(null);
+
+  async function onPress() {
+      const newResult = await Calculator?.add(3,7);
+      setResult(newResult ?? null);
+  }
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+      <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+      <Text style={{ "margin":20 }}>3+7={result ?? "??"}</Text>
+      <Button title="Compute" onPress={onPress} />
+      </SafeAreaView>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  export default App;
