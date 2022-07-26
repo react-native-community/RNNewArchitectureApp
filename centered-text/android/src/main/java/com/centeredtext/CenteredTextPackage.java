75e6bd56d623a18d16f49816ed23a4f1bed767e8
Riccardo Cipolleschi <cipolleschi@fb.com>
2022-07-26 11:59:27 +0100

    [Fabric Components - Android] Create Android Implementation

---
+++ centered-text/android/src/main/java/com/centeredtext/CenteredTextPackage.java
@@ -0,0 +1,22 @@
+package com.centeredtext;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CenteredTextPackage implements ReactPackage {
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        List<ViewManager> viewManagers = new ArrayList<>();
+        viewManagers.add(new CenteredTextManager(reactContext));
+        return viewManagers;
+    }
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
