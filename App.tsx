2bfee144d9c13e81a04b16aafc9f921317af3f34
Christoph Purrer <christophpurrer@gmail.com>
2022-11-29 02:13:55 +0100

    [JS] Use C++ Turbo Native Module in App

--- App.tsx
+++ App.tsx
@@ -24,6 +24,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import NativeSampleModule from './tm/NativeSampleModule';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -76,6 +77,9 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Section title="Cxx TurboModule">
+            NativeSampleModule.reverseString(...) = {NativeSampleModule.reverseString("the quick brown fox jumps over the lazy dog")}
+          </Section>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
