b6397aea19919aaf0492c21b414c1104dae95818
Samuel Susla <samuel.susla@gmail.com>
2023-02-02 15:58:27 +0000

    Add more examples

--- AwesomeApp/App.tsx
+++ AwesomeApp/App.tsx
@@ -8,10 +8,41 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View, Image} from 'react-native';
 
+
+function Movie(props) {
+  return (
+    <View style={{ padding: 10, flexDirection: 'row', alignItems: 'center' }}>
+      <View style={{ flexDirection: 'row', alignItems:'center' }}>
+        <View style={{ width: 60, height: 60, backgroundColor: 'blue' }} />
+        <View style={{marginLeft: 10}}>
+          <Text>{props.title}</Text>
+          <Text style={{color: 'grey'}}>{props.genre}</Text>
+        </View>
+      </View>
+      <View style={{marginLeft: 'auto'}}>
+        <Text>{props.time}</Text>
+        <Text>{props.place}</Text>
+      </View>
+    </View>
+  );
+}
+
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <View>
+      <Movie title="Movie 1" genre="Drama" time="17:00" place="Theater A" />
+
+      {/* <View style={{flexDirection:'row'}}>
+        <View>
+          <Text>Alice</Text>
+          <Text>Company A</Text>
+        </View>
+        <View>
+          <Text>Bob</Text>
+          <Text>Company B</Text>
+        </View>
+      </View> */}
+      {/* <View>
         <View style={styles.margin10}>
           <View style={styles.padding10}>
             <Image
@@ -22,7 +53,7 @@ function App(): JSX.Element {
             <Text>This is a title</Text>
           </View>
          </View>
-      </View>
+      </View> */}
     </SafeAreaView>
   );
 }
